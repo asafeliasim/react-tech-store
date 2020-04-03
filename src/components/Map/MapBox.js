8103bd3945eeeb8789b3df46b0aa4d1fed2ed900
asafeliasim <asafeliasim@gmail.com>
2020-04-03 19:43:10 +0300

    fix map

--- src/components/Map/MapBox.js
+++ src/components/Map/MapBox.js
@@ -5,8 +5,10 @@ import axios from 'axios';
 const api = 'pk.eyJ1IjoiYXNhZmVsaWFzaW0iLCJhIjoiY2szbmV6bjN2MDlodzNkbzZxMm5hMXdzMyJ9.8N4XyIX5y-SyphxY8DJTBw';
 
 export default function MapBox(){
-    //Here i initialize properties of the map
+
    const [viewPort, setViewport] = useState({
+      latitude:32.075250,
+      longitude:34.804720,
       width:'100vw',
       height:'400px',
       zoom: 16,
